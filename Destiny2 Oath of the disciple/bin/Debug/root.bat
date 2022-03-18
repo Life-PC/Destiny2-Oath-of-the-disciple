9d896bb48edb28aceaa67a7f46ae9af200ec1c9a
Life-PC <lifepc.iynx@gmail.com>
2022-03-18 18:09:42 +0900

    Delete Destiny2 Oath of the disciple directory

--- Destiny2 Oath of the disciple/bin/Debug/root.bat
+++
@@ -1,3 +0,0 @@
-netsh http add urlacl url=http://+:80/ user=everyone
-netsh advfirewall firewall add rule name="Destiny2 Oath of the disciple" dir=in action=allow
-netsh advfirewall firewall set rule name="Destiny2 Oath of the disciple" new program=system profile=private protocol=tcp localport=80
